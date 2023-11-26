79241fb7061e5da78868e4b4a6884b650d03cbc8
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-11-26 11:41:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ class Database:
 
     '''CREATE the Tasks TABLE'''
 
-    def create_task_table(self):
+    def create_task_table(self):                                                                                               
 
         self.cursor.execute(
             "CREATE TABLE IF NOT EXISTS tasks(id integer PRIMARY KEY AUTOINCREMENT, task varcahr(50) NOT NULL, due_date varchar(50), completed BOOLEAN NOT NULL CHECK (completed IN (0, 1)))")
